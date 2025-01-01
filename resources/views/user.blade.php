e0296319b24a86097cd7cbdac8a433eaf9bedb89
Stefan Marjanovic <stefan.marjanovic@freighttrackertms.com>
2025-01-01 22:01:04 +1100

    add custom css
    modal header
    button
    theme color

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -32,7 +32,7 @@
         <div class="modal fade" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="addUserModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                    <div class="modal-header">
+                    <div class="modal-header modal-header">
                         <h5 class="modal-title" id="addUserModalLabel">Add User</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
